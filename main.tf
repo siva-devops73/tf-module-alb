f9de6dab69084c6af507a737553a86cc1a09c128
Siva Kumar <sivakumar9399959803@gmail.com>
2023-08-13 08:36:03 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -34,3 +34,19 @@ resource "aws_lb" "main" {
 
   tags               = merge({ Name = "${var.name}-${var.env}" }, var.tags)
 }
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "500"
+    }
+  }
+}
