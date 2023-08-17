38058f9f6c7e0d85025885b6caf9c68ad63e97dc
Siva Kumar <sivakumar9399959803@gmail.com>
2023-08-17 09:51:13 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "main" {
 
 
 resource "aws_lb" "main" {
-  name               = "${var.name}-${var.env}alb"
+  name               = "${var.name}-${var.env}-alb"
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
   security_groups    = [aws_security_group.main.id]
