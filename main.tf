2ee89d725dad1077646a556ce6afe4e8d42c69f0
Siva Kumar <sivakumar9399959803@gmail.com>
2023-09-01 09:25:35 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -44,14 +44,14 @@ resource "aws_lb" "main" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
-  port              =  "80"
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type = "redirect"
 
     redirect {
-      port        =  "443"
+      port        = "443"
       protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
