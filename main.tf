9fccb392c0d0b72b0500fa0a880ad20c61a96101
Siva Kumar <sivakumar9399959803@gmail.com>
2023-10-05 15:23:40 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -42,7 +42,8 @@ resource "aws_lb" "main" {
   tags               = merge({ Name = "${var.name}-${var.env}-lb" }, var.tags)
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "public" {
+  count             = var.name == "public" ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
@@ -58,7 +59,25 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+resource "aws_lb_listener" "private" {
+  count             = var.name == "private" ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "500"
+    }
+  }
+}
+
 resource "aws_lb_listener" "main" {
+  count             = var.name == "public" ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              =  443
   protocol          = "HTTPS"
